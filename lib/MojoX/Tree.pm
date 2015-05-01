3d39349cf77030ec047b1857d7e16c600c7c1e93
Kostya Ten <kostya@yandex-team.ru>
2015-05-01 19:38:38 +0400

    first commit

---
+++ lib/MojoX/Tree.pm
@@ -0,0 +1,212 @@
+package MojoX::Tree;
+use Mojo::Base -base;
+use Mojo::Util qw(dumper);
+use Mojo::Collection 'c';
+use DBI;
+use Carp qw(croak);
+  
+our $VERSION  = '0.01';
+
+sub new {
+	my $class = shift;
+	my %args = @_;
+
+	my $config = {};
+	if(exists $args{'mysql'} && $args{'mysql'} && ref $args{'mysql'} eq 'MojoX::Mysql'){
+		$config->{'mysql'} = $args{'mysql'};
+	}
+	else{
+		croak qq/invalid MojoX::Mysql object/;
+	}
+
+	if(exists $args{'table'} && $args{'table'} && $args{'table'} =~ m/^[0-9a-z_-]+$/){
+		$config->{'table'} = $args{'table'};
+	}
+	else{
+		croak qq/invalid table/;
+	}
+
+	if(exists $args{'column'} && $args{'column'} && ref $args{'column'} eq 'HASH'){
+		$config->{'column'} = $args{'column'};
+	}
+	else{
+		croak qq/invalid column/;
+	}
+
+	if(exists $args{'length'} && $args{'length'} && $args{'length'} =~ m/^[0-9]+$/){
+		$config->{'length'} = $args{'length'};
+	}
+	else{
+		croak qq/invalid column/;
+	}
+
+
+	return $class->SUPER::new($config);
+}
+
+sub mysql {
+	return shift->{'mysql'};
+}
+
+sub add_item {
+	my ($self,$name,$parent_id) = @_;
+
+	my $table = $self->{'table'};
+	my $column_id        = $self->{'column'}->{'id'};
+	my $column_name      = $self->{'column'}->{'name'};
+	my $column_path      = $self->{'column'}->{'path'};
+	my $column_level     = $self->{'column'}->{'level'};
+	my $column_parent_id = $self->{'column'}->{'parent_id'};
+
+	my $parent_path = undef;
+	if(defined $parent_id && $parent_id){
+		my $get_id = $self->get_id($parent_id);
+		if(defined $get_id){
+			$parent_path = $get_id->{'path'};
+		}
+		else{
+			croak "invalid parent_id:$parent_id";
+		}
+	}
+
+	# Создаем запись
+	my ($insertid,$counter) = $self->mysql->do("INSERT INTO `$table` (`$column_name`) VALUES (?)", $name);
+
+	# Формируем материлизованный путь
+	my $path = $self->make_path($insertid);
+
+	$path = $parent_path.$path if(defined $parent_path);
+	my $level = $self->make_level($path); # Узнает текущий уровень
+
+	$self->mysql->do(
+		"UPDATE `$table` SET `$column_path` = ?, `$column_level` = ?, `$column_parent_id` = ? WHERE `$column_id` = ?;",
+		$path,$level,$parent_id,$insertid
+	);
+	return $insertid;
+}
+
+# Удаляет текущего элемент и детей
+sub delete_item {
+	my ($self,$id) = @_;
+
+	my $path = undef;
+	my $get_id = $self->get_id($id);
+	if(defined $get_id){
+		$path = $get_id->{'path'};
+	}
+	else{
+		croak "invalid id:$id";
+	}
+
+	my $table       = $self->{'table'};
+	my $column_path = $self->{'column'}->{'path'};
+	my ($insertid,$counter) = $self->mysql->do("DELETE FROM `$table` WHERE `$column_path` LIKE '$path%';");
+	if($counter > 0){
+		return $counter;
+	}
+	else{
+		croak "Unable to delete";
+	}
+}
+
+sub move_item {
+	my ($self,$id,$target_id) = @_;
+	my $table = $self->{'table'};
+	my $column_id        = $self->{'column'}->{'id'};
+	my $column_name      = $self->{'column'}->{'name'};
+	my $column_path      = $self->{'column'}->{'path'};
+	my $column_level     = $self->{'column'}->{'level'};
+	my $column_parent_id = $self->{'column'}->{'parent_id'};
+
+	my $get_id = $self->get_id($id);
+	croak "invalid id:$id" if(!defined $id);
+
+	my $get_target_id = $self->get_id($target_id);
+	croak "invalid id:$get_target_id" if(!defined $get_target_id);
+
+	croak "Impossible to transfer to itself or children" if($id eq $target_id);
+
+	my $path        = $get_id->{'path'};
+	my $path_target = $get_target_id->{'path'};
+	croak "Impossible to transfer to itself or children" if($path =~ m/^$path_target/);
+
+	my $collection = $self->mysql->query("SELECT `$column_id` as `id`, `$column_path` as `path` FROM `$table` WHERE `$column_path` LIKE '$path%';");
+	$collection->each(sub {
+		my $e = shift;
+		my $id   = $e->{'id'};
+		if($e->{'path'} =~ m/(?<path>($path\d*))/g){
+			say dumper $path_target.$+{'path'};
+			#my $level           = $self->_level($path_target.$+{'path'});
+			#my $parent_id       = $self->_parent_id($path_target.$+{'path'});
+		}
+	});
+
+
+
+	#$self->get_id($target_id);
+}
+
+sub make_path {
+	my ($self,$id) = @_;
+	my $length = $self->{'length'};
+	my $length_id = length $id;
+	if($length_id < $length){
+		my $zero = '0' x ($length - $length_id);
+		$id = $zero.$id;
+	}
+	return $id;
+}
+
+sub make_level {
+	my ($self,$path) = @_;
+	my $length = $self->{'length'};
+	my @counter = ($path =~ m/([0-9]{$length})/g);
+	return scalar @counter;
+}
+
+
+# Получение очереди по id
+sub get_id {
+	my ($self,$id) = @_;
+
+	my $table = $self->{'table'};
+	my $column_id        = $self->{'column'}->{'id'};
+	my $column_name      = $self->{'column'}->{'name'};
+	my $column_path      = $self->{'column'}->{'path'};
+	my $column_level     = $self->{'column'}->{'level'};
+	my $column_parent_id = $self->{'column'}->{'parent_id'};
+
+	my ($collection,$counter) = $self->mysql->query("SELECT `$column_id`, `$column_path`, `$column_name`, `$column_level`, `$column_parent_id` FROM `$table` WHERE `$column_id` = ? LIMIT 1", $id);
+	return if($counter eq '0E0');
+
+	my $result = $collection->last;
+
+	# Получаем всех детей
+	my $path = $result->{'path'};
+	my $level = $result->{'level'};
+	$result->{'children'} = $self->mysql->query("
+		SELECT `$column_id`, `$column_path`, `$column_name`, `$column_level`, `$column_parent_id` FROM `$table`
+		WHERE `$column_path` LIKE '$path%' AND `level` != ?
+	",$level);
+
+	# Получаем всех родителей
+	my @parent = ();
+	my $length = $self->{'length'};
+	for(($path =~ m/(\d{$length})/g)){
+		push(@parent, int $_);
+	}
+	@parent = grep(!/$id/, @parent);
+	my $parent = join(",",@parent);
+
+	if(defined $parent && $parent){
+		$parent = $self->mysql->query("SELECT `$column_id`, `$column_path`, `$column_name`, `$column_level`, `$column_parent_id` FROM `$table` WHERE `$column_id` IN($parent)");
+	}
+	else {
+		$parent = c();
+	}
+
+	$result->{'parent'} = $parent;
+	return $result;
+}
+
+1;
---
+++ t/basic.t
@@ -0,0 +1,84 @@
+use Mojo::Base -strict;
+use Test::More;
+use Mojolicious::Lite;
+use Test::Mojo;
+use Mojo::Util qw(dumper);
+use FindBin;
+use lib "$FindBin::Bin/../lib/";
+use MojoX::Mysql;
+use MojoX::Tree;
+
+my %config = (
+	user=>'root',
+	password=>undef,
+	server=>[
+		{dsn=>'database=test;host=localhost;port=3306;mysql_connect_timeout=5;', type=>'master'},
+		{dsn=>'database=test;host=localhost;port=3306;mysql_connect_timeout=5;', type=>'slave'},
+		{dsn=>'database=test;host=localhost;port=3306;mysql_connect_timeout=5;', id=>1, type=>'master'},
+		{dsn=>'database=test;host=localhost;port=3306;mysql_connect_timeout=5;', id=>1, type=>'slave'},
+		{dsn=>'database=test;host=localhost;port=3306;mysql_connect_timeout=5;', id=>2, type=>'master'},
+		{dsn=>'database=test;host=localhost;port=3306;mysql_connect_timeout=5;', id=>2, type=>'slave'},
+	]
+);
+$config{'user'} = 'travis' if(defined $ENV{'USER'} && $ENV{'USER'} eq 'travis');
+
+my $mysql = MojoX::Mysql->new(%config);
+my $tree = MojoX::Tree->new(mysql=>$mysql, table=>'tree', length=>10, column=>{id=>'tree_id', name=>'name', path=>'path', level=>'level', parent_id=>'parent_id'});
+
+# Удаляем таблицу
+$mysql->do("DROP TABLE IF EXISTS `tree`;");
+
+my $sql = qq/
+	CREATE TABLE `tree` (
+		`tree_id` int(14) unsigned NOT NULL AUTO_INCREMENT,
+		`name` varchar(255) NOT NULL,
+		`path` mediumtext NOT NULL,
+		`level` int(14) unsigned NOT NULL,
+		`parent_id` int(14) unsigned NULL,
+		PRIMARY KEY (`tree_id`),
+		KEY `path` (`path`(30))
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/;
+$mysql->do($sql); # создаем таблицу
+
+my $id = $tree->add_item('test');
+ok($id == 1,'check id');
+
+my $result = $tree->get_id($id);
+ok($result->{'level'} == 1, 'level');
+ok($result->{'name'} eq 'test', 'name');
+ok($result->{'path'} eq '0000000001', 'path');
+ok($result->{'tree_id'} == 1, 'tree_id');
+ok(ref $result->{'children'} eq 'Mojo::Collection', 'children');
+ok(ref $result->{'parent'} eq 'Mojo::Collection', 'parent');
+
+
+$id = $tree->add_item('тест',$id);
+$result = $tree->get_id($id);
+ok($result->{'path'} eq '00000000010000000002', 'path');
+ok($result->{'parent_id'} == 1, 'parent_id');
+ok($result->{'tree_id'} == 2, 'tree_id');
+ok($result->{'name'} eq 'тест', 'name');
+ok(ref $result->{'parent'} eq 'Mojo::Collection', 'parent');
+ok($result->{'parent'}->size == 1, 'parent size');
+
+$result = $tree->get_id(1);
+ok($result->{'children'}->size == 1, 'children size');
+
+$result = $tree->delete_item(1);
+ok($result == 2, 'delete_item');
+
+
+
+
+#$tree->add_item('test',2);
+#my $id = $tree->add_item('new root');
+#$tree->move_item(2,$id);
+#say dumper $tree->get_id(2);
+
+$mysql->db->commit;
+
+
+
+done_testing();
+
